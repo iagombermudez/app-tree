6bb84767ebd0e7eeb6ca7fe0ec8049e442dc493a
Iago Bermúdez <iagombermudez@gmail.com>
2025-06-23 19:44:09 +0200

    chore: rename app_lead to actions

--- src/commands.rs
+++ src/commands.rs
@@ -22,33 +22,33 @@ pub fn read_app_command() -> AppCommand {
 }
 
 pub fn execute_add() -> Result<(), String> {
-    let apps_result = config::read_config();
-    match apps_result {
-        Ok(mut apps) => {
+    let actions_result = config::read_config();
+    match actions_result {
+        Ok(mut actions) => {
             // Read the app_name and executable arguments
             let action_name = std::env::args()
                 .nth(2)
-                .expect("App name parameter is missing");
+                .expect("Action name parameter is missing");
             let action_command = std::env::args()
                 .nth(3)
                 .expect("Executable parameter is missing");
 
             // Check if the app already exists
-            let action_exists: bool = apps.iter().any(|app| app.name == action_name);
+            let action_exists: bool = actions.iter().any(|app| app.name == action_name);
             if action_exists {
                 return Err(format!("Command {} already exists", action_name));
             }
             //Create new AppLeaf
-            let app_leaf = Action {
+            let action = Action {
                 name: action_name,
                 command: action_command,
             };
 
             // Add the new instance of app_leaf to apps
-            apps.push(app_leaf);
+            actions.push(action);
 
             // Save the new app in the configuration file
-            let save_result = config::write_config(apps);
+            let save_result = config::write_config(actions);
             return save_result;
         }
         Err(e) => Err(format!("Error {}", e)),
