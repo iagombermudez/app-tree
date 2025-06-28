b700507e4a6b1d179c6d66a2c7c5e3206ab1c936
Iago Bermúdez <iagombermudez@gmail.com>
2025-06-28 23:02:29 +0200

    return a new struct when parsing JSON instead of passing array as reference

--- src/config.rs
+++ src/config.rs
@@ -36,7 +36,7 @@ fn parse_to_actions(actions: &mut Vec<ActionComponent>, action: &ActionJSON) {
     }
 }
 
-pub fn write_config(config: Vec<ActionComponent>) -> Result<(), String> {
+pub fn write_config(actions: Vec<ActionComponent>) -> Result<(), String> {
     let mut apps_file = match File::options()
         .read(true)
         .write(true)
@@ -47,7 +47,8 @@ pub fn write_config(config: Vec<ActionComponent>) -> Result<(), String> {
         Err(_) => File::create("app_config.json").expect("File could not be created"),
     };
 
-    let result_writter_json = serde_json::to_writer(&mut apps_file, &config);
+    let actions_json = parse_to_json(&actions);
+    let result_writter_json = serde_json::to_writer(&mut apps_file, &actions_json);
     return match result_writter_json {
         Ok(_) => {
             let mut writer = BufWriter::new(apps_file);
@@ -59,4 +60,30 @@ pub fn write_config(config: Vec<ActionComponent>) -> Result<(), String> {
         }
         Err(_) => Err(String::from("File could not be parsed to json")),
     };
+}
+
+fn parse_to_json(actions: &Vec<ActionComponent>) -> Vec<ActionJSON> {
+    let mut actions_json: Vec<ActionJSON> = vec![];
+    for action in actions.iter() {
+        match &action {
+            ActionComponent::Leaf(leaf) => {
+                let action_json = ActionJSON {
+                    name: leaf.name.clone(),
+                    command: Some(leaf.command.clone()),
+                    actions: None,
+                };
+                actions_json.push(action_json);
+            }
+            ActionComponent::Component(component) => {
+                let inner_actions: Vec<ActionJSON> = parse_to_json(&component.actions);
+                let action_json = ActionJSON {
+                    name: component.name.clone(),
+                    command: None,
+                    actions: Some(inner_actions),
+                };
+                actions_json.push(action_json)
+            }
+        }
+    }
+    return actions_json;
 }
