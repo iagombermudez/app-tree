6075c01ce7d21a4c68cdcf4ad311b0dc526652a6
Iago Bermúdez <iagombermudez@gmail.com>
2025-07-04 09:49:54 +0200

    display a message when using the list command and there are no actions

--- src/commands/command_list.rs
+++ src/commands/command_list.rs
@@ -7,13 +7,18 @@ pub fn execute_list() -> Result<(), String> {
     let actions_result = config::read_config();
     return match actions_result {
         Ok(actions) => {
-            println!("NAME --- ACTION");
-            for action in actions.iter() {
-                match action {
-                    ActionComponent::Leaf(leaf) => println!("{}", leaf.to_string(0)),
-                    ActionComponent::Branch(branch) => println!("{}", branch.to_string(0)),
+            if actions.len() == 0 {
+                println!("No actions. Add an action by using the <add> command");
+            } else {
+                println!("NAME --- ACTION");
+                for action in actions.iter() {
+                    match action {
+                        ActionComponent::Leaf(leaf) => println!("{}", leaf.to_string(0)),
+                        ActionComponent::Branch(branch) => println!("{}", branch.to_string(0)),
+                    }
                 }
             }
+
             Ok(())
         }
         Err(e) => Err(format!("{e}")),
