f37f313b5d0f73ab872d43466a5961e10034cd3e
Iago Bermúdez <iagombermudez@gmail.com>
2025-07-05 10:17:07 +0200

    add list and open command help

--- src/commands/command_help.rs
+++ src/commands/command_help.rs
@@ -13,16 +13,37 @@ List of commands:
 <add>:      Useful for adding a new action. You can either add an action as a standalone using 
             'app-tree add <name-of-action> <command>', or you can add it inside a group of actions
             using 'app-tree add <name-of-branch> <name-of-action> <commmand>.
+            Params (they must be given in order):
+                <name-of-branch>: (optional) the name of the branch the action will be stored in
+                <name-of-action>: the name of the action to be added
+                <command>: the command that will be executed when calling the action
             Example: 
                 $ app-tree add repositories my-awesome-project 'nvim ~/Source/repositories/my-awesome-project'
 
 <remove>:   Removes an action. Follows the same pattern as the <add> command, but it is not necessary to include the 
             action command. 
+            Params (they must be given in order):
+                <name-of-branch>: (optional) the name of the branch the action will be stored in
+                <name-of-action>: the name of the action to be added
             Examples:
                 $ app-tree remove repositories my-awesome-project 
                 $ app-tree remove repositories //removes all the actions and the branch
                 $ app-tree remove my-executable //if my executable is a standalone project, it removes it 
-                "
+
+<list>:     List all the actions that have been added.
+            Example:
+                $ app-tree list
+
+<open>:     Executes a given action or a set of actions if the parameter given is a branch.
+            Params (they must be given in order). Both parameters are optional, but at least one must be given: 
+                <name-of-branch>: (optional) the name of the branch the action will be stored in
+                <name-of-action>: (optional) the name of the action to be added
+            Examples:
+                $ app-tree open repositories my-awesome-project 
+                $ app-tree open repositories //open all the actions and the branch
+                $ app-tree open my-executable //if my executable is a standalone project, it opens it 
+
+"
         )
     );
     return Ok(());
